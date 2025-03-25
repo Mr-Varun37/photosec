a2f232805f0238d458f6bf4f7756dc8b4934623d
Mr-Varun37 <varunpachchigar30@gmail.com>
2025-03-25 13:05:04 +0530

    first commit

---
+++ footer.php
@@ -0,0 +1,31 @@
+  <!-- footer section -->
+  <footer class="footer_section">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-xl-6">
+          <p>
+            &copy; <span id="displayYear"></span> All Rights Reserved. Design
+            by
+            <a href="https://html.design/">DroidNest</a>
+          </p>
+        </div>
+        <div class="col-xl-6">
+          <div class="link_box">
+            <a class="" href="index.php">
+              Home
+            </a>
+            <a class="" href="about.php">
+              About
+            </a>
+            <a class="" href="portfolio.php">
+              Portfolio
+            </a>
+            <a class="" href="team.php">
+              Team
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!-- footer section -->
---
+++ header.php
@@ -0,0 +1,44 @@
+    <!-- header section strats -->
+    <header class="header_section">
+      <nav class="navbar navbar-expand-lg custom_nav-container">
+        <div class="custom_menu-btn">
+          <button onclick="openNav()">
+            <span class="s-1"> </span>
+            <span class="s-2"> </span>
+            <span class="s-3"> </span>
+          </button>
+        </div>
+        <div id="myNav" class="overlay">
+          <div class="menu_btn-style ">
+            <button onclick="closeNav()">
+              <span class="s-1"> </span>
+              <span class="s-2"> </span>
+              <span class="s-3"> </span>
+            </button>
+          </div>
+          <div class="overlay-content">
+            <a class="active" href="index.php">
+              Home
+            </a>
+            <a class="" href="about.php">
+              About
+            </a>
+            <a class="" href="portfolio.php">
+              Portfolio
+            </a>
+            <a class="" href="team.php">
+              Team
+            </a>
+          </div>
+        </div>
+        <a class="navbar-brand" href="index.php">
+          <span>
+            Photosec
+          </span>
+        </a>
+        <a href="" class="call_btn">
+          Call Us Now
+        </a>
+      </nav>
+    </header>
+    <!-- end header section -->
